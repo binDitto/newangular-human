eff88f51ad8c8faf052e2b5393fc6cec8445f7da
Bin Tran <sudocode.bin@gmail.com>
2017-05-30 18:43:41 -0700

    node server created with server.js file"

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var morgan = require('morgan');
+var path = require('path');
+var mongoose = require('mongoose');
+
+var app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true 
+
+}));
+
+//PORT and Database Connection
+var PORT = 8000 || process.env.PORT;
+var DB = 'localhost:27017/newangular-human';
+mongoose.connect(DB, function(err){
+    if(err){
+        return err;
+    }
+    else {
+        console.log('Successfully connected to ' + DB);
+    }
+});
+
+//Setting view engine to display the views
+app.set('views', __dirname + '/client/views');
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/client'));
+
+//Listen in on the connection
+app.listen(PORT, function(){
+    console.log('Listening on port ' + PORT);
+});
